56b58be184404726f363629b52979a0c0e0e867f
Piotr Kołodziejczyk <contact@frontend.london>
2019-03-05 21:34:15 +0000

    showing and hiding timer and timer editor

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,19 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import DisplayTime from './DisplayTime';
 import EditTime from './EditTime';
 import './../styles/timer.scss';
 
-const Timer = () => (
-  <div className="timer">
-    <h2>Timer</h2>
-    {/* <DisplayTime /> */}
-    <EditTime />
+const Timer = () => {
+  const [editing, setEditing] = useState(false);
+  const stopEditing = () => setEditing(false);
+  const startEditing = (e) => {
+    e.stopPropagation();
+    setEditing(true);
+  };
 
-    <div className="buttons">
-      <button type="button" className="btn btn-primary">Start</button>
-      <button type="button" className="btn btn-secondary">Reset</button>
+  return (
+    <div className="timer">
+
+      <div className="card">
+        <div className="card-body" onClick={stopEditing}>
+          <h2>Timer</h2>
+          {editing ? (
+            <EditTime />
+          ) : (
+              <h1 className="display-time" onClick={startEditing}>
+                <DisplayTime />
+              </h1>
+            )}
+        </div>
+        <hr />
+        <div className="buttons">
+          <button type="button" className="btn btn-primary">Start</button>
+          <button type="button" className="btn btn-secondary">Reset</button>
+        </div>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Timer;
